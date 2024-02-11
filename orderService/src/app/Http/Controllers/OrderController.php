a37bb1206c920707ddd9ff9c1b01fbe942c3bf65
root <root@pop-os.localdomain>
2024-02-11 19:46:09 +0600

    order service dockerize and model create

---
+++ orderService/src/app/Http/Controllers/OrderController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+   protected Model $model;
+
+   public function __construct(Order $model)
+   {
+       $this->model = $model;
+   }
+
+   public function store(Request $request)
+   {
+       $order =$this->model->create($request->all());
+       return response()->json([
+            'data' => $order
+       ]);
+   }
+}
